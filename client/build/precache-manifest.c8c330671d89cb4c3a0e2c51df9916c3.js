0a2c6503b530d1945cfcc9d4e19815fbe4ea2459
khafrewatkins <khafrewatkins@gmail.com>
2021-09-01 20:48:55 -0500

    Add files via upload

---
+++ client/build/precache-manifest.c8c330671d89cb4c3a0e2c51df9916c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef0e1719ab8020d24fd924de9fa92cea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73d277fccb1a9c712372",
+    "url": "/static/css/2.a389d3b1.chunk.css"
+  },
+  {
+    "revision": "73d277fccb1a9c712372",
+    "url": "/static/js/2.0326c445.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.0326c445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5720e6af6918c4d1a502",
+    "url": "/static/js/main.b649a607.chunk.js"
+  },
+  {
+    "revision": "0939b0f714c2b1ff39ed",
+    "url": "/static/js/runtime-main.6167e78a.js"
+  }
+]);
